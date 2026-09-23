b5ce243cf7d24f6312eec753a018db49eac838f8
ElizabethPython-cloud <gazerlucy@gmail.com>
2026-09-23 19:51:37 +0800

    Add files via upload

---
+++ PYTHON/13 Python Lists/7.List Comprehension/2-The Syntax/1)Condition.py
@@ -0,0 +1,2 @@
+newlist = [x for x in fruits if x != "apple"]
+newlist = [x for x in fruits]
---
+++ PYTHON/13 Python Lists/7.List Comprehension/2-The Syntax/3)Expression.py
@@ -0,0 +1,3 @@
+newlist = [x.upper() for x in fruits]
+newlist = ['hello' for x in fruits]
+newlist = [x if x != "banana" else "orange" for x in fruits]
